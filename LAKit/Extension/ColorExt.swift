e39dbde6ec4db9f55fb158784ac16cce239e8f06
liuao <309225529@qq.com>
2018-01-27 18:31:45 +0800

    .

--- LAKit/Extension/ColorExt.swift
+++ LAKit/Extension/ColorExt.swift
@@ -6,7 +6,7 @@
 import UIKit
 
 extension UIColor {
-    
+
     //用数值初始化颜色，便于生成设计图上标明的十六进制颜色
     convenience init(_ valueRGB: UInt,_ alpha: CGFloat = 1.0) {
         self.init(red: CGFloat((valueRGB & 0xFF0000) >> 16) / 255.0, green: CGFloat((valueRGB & 0x00FF00) >> 8) / 255.0, blue: CGFloat(valueRGB & 0x0000FF) / 255.0, alpha: alpha)
